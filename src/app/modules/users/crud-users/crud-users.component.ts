99ff9b5ec51fb71506d92a356cd19a971f9c656b
Jonathan Maldonado <jmmaldon@bancolombia.com.co>
2024-08-01 17:40:51 -0500

    primer commit

---
+++ src/app/modules/users/crud-users/crud-users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-users.component.html'
+})
+export class CrudUsersComponent {
+
+}
